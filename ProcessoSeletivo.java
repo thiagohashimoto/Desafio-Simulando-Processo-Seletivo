bc54a0275631d670d090c86bda8cdfe0fc068248
PC Thiago <thiago.hashimoto94@gmail.com>
2023-07-03 09:33:51 -0300

    first commit

---
+++ ProcessoSeletivo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProcessoSeletivo{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch(ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+	}
+}
+
